42e4bdce3c7cb09b857e41cae2f6db47a19e3350
myageltech <myagel@campus.technion.ac.il>
2023-04-30 20:15:29 +0300

    with push

--- Commands.h
+++ Commands.h
@@ -156,7 +156,7 @@ public:
   map<int, JobEntry> jbs_map;
   // TODO: Add your data members
 public:
-  JobsList();
+  JobsList() = default;
   ~JobsList() = default;
   void addJob(Command *cmd, bool isStopped = false);
   void printJobsList();
