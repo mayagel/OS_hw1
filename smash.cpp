9059223858126475e692e908f45cc0259b0eb9fd
myageltech <myagel@campus.technion.ac.il>
2023-05-01 19:04:33 +0300

    mam2

--- smash.cpp
+++ smash.cpp
@@ -7,7 +7,7 @@
 
 int main(int argc, char *argv[])
 {
-    // std::cout << "start function\n";
+    std::cout << "start function\n";
     if (signal(SIGTSTP, ctrlZHandler) == SIG_ERR)
     {
         perror("smash error: failed to set ctrl-Z handler!!!!!!!\n");
@@ -16,14 +16,14 @@ int main(int argc, char *argv[])
     {
         perror("smash error: failed to set ctrl-C handler\n");
     }
-    // std::cout << "after signals";
+    std::cout << "after signals";
     // TODO: setup sig alarm handler
 
     SmallShell &smash = SmallShell::getInstance();
-    // std::cout << "after creating smash\n";
+    std::cout << "after creating smash\n";
     while (true)
     {
-        // std::cout << "inside while\n";
+        std::cout << "inside while\n";
 
         std::cout << smash.getPrompt() << "> ";
         std::string cmd_line;
