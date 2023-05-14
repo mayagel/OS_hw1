bc51b5d6c757f521a3b4845b89dafe43da86de39
myageltech <myagel@campus.technion.ac.il>
2023-05-14 17:20:39 +0300

    fix conf

--- signals.cpp
+++ signals.cpp
@@ -7,11 +7,8 @@ using namespace std;
 
 void ctrlZHandler(int sig_num)
 {
-<<<<<<< Updated upstream
   std::shared_ptr<Command> curr_cmd = SmallShell::getInstance().getCurrentCmd();
-=======
-  Command *curr_cmd = SmallShell::getInstance().getCurrentCmd();
->>>>>>> Stashed changes
+
   if (curr_cmd)
   {
     kill(curr_cmd->getPid(), SIGSTOP);
@@ -20,27 +17,17 @@ void ctrlZHandler(int sig_num)
     cout << "smash: process " << int(curr_cmd->getPid()) << " was stopped" << endl;
   }
   cout << "smash: got ctrl-Z" << endl;
-<<<<<<< Updated upstream
   return;
-=======
->>>>>>> Stashed changes
 }
 
 void ctrlCHandler(int sig_num)
 {
   cout << "smash: got ctrl-C" << endl;
-<<<<<<< Updated upstream
   std::shared_ptr<Command> curr_cmd = SmallShell::getInstance().getCurrentCmd();
   if (curr_cmd)
   {
     kill(curr_cmd->getPid(), SIGINT);
     cout << "smash: process " << int(curr_cmd->getPid()) << " was killed" << endl;
-=======
-  Command *curr_cmd = SmallShell::getInstance().getCurrentCmd();
-  if (curr_cmd)
-  {
-    kill(curr_cmd->getPid(), SIGINT);
->>>>>>> Stashed changes
     SmallShell::getInstance().setCurrentCmd(nullptr);
     cout << "smash: process " << int(curr_cmd->getPid()) << " was killed" << endl;
   }
